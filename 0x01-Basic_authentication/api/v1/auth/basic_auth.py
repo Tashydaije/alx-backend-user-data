b1471b19097441c310d3637235684a940f35c49e
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-10 16:26:19 +0000

    returns user email n password from decode value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,17 @@ class BasicAuth(Auth):
             return decoded_token.decode('utf-8')
         except Exception as error:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """Extracts user credentials from auth token"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_credentials = decoded_base64_authorization_header.split(':', 1)
+        email, password = user_credentials
+        return email, password
