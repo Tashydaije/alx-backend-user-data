5ab53277795557c5b2d2af086062388d89e3ad43
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-10 16:15:50 +0000

    Basic = Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,3 +15,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ return decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            from base64 import b64decode
+            decoded_token = b64decode(base64_authorization_header,
+                                      validate=True)
+            return decoded_token.decode('utf-8')
+        except Exception as error:
+            return None
