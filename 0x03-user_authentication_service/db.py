68473ba2b9fb5bf574490980699ee0293dfdc8fe
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-18 23:18:42 +0000

    ensure required fields

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,6 +34,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database and return the User object
         """
+        if not email or not hashed_password:
+            return
         new_user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(new_user)
