99fa6b57420a9bd0bd46ac093487047c29d7a2cf
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-18 23:49:41 +0000

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,3 +7,8 @@ import bcrypt
 def _hash_password(password: str) -> bytes:
     """Encrypts a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a unique ID"""
+    return str(uuid4())
