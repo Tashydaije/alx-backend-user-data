b6207e9cddbcdade501220d1dae25c9e588068f7
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-18 23:46:24 +0000

    oops

--- 0x03-user_authentication_service/main_4.py
+++ 0x03-user_authentication_service/main_4.py
@@ -5,6 +5,3 @@ Main file
 from auth import _hash_password
 
 print(_hash_password("Hello Holberton"))
-
-bob@dylan:~$ python3 main.py
-b'$2b$12$eUDdeuBtrD41c8dXvzh95ehsWYCCAi4VH1JbESzgbgZT.eMMzi.G2'
