b873eca29e472481ac95444c6d01842ff1a9bcfd
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-19 00:04:41 +0000

    start simple flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Basic Flask API"""
+from flask import (
+    Flask,
+    jsonify
+)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """Entry point of API"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
