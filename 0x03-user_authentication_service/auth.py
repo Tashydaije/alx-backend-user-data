a3f1d4e8d28d1bb43d3e6889fa51102f45b75283
Tashydaije <mbuguanatasha01@gmail.com>
2024-07-19 00:00:24 +0000

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 """Authentication module"""
 import bcrypt
 import uuid
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +16,19 @@ def _hash_password(password: str) -> bytes:
 def _generate_uuid() -> str:
     """Generate a unique ID"""
     return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
